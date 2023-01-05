7a3d96f928a762440148dec67daa3922fc5dee70
RenanCena <rcdm.snf18@uea.edu.br>
2023-01-05 01:02:01 -0400

    Update (borders)
    creating borders

--- pygame/breakout/breakout.py
+++ pygame/breakout/breakout.py
@@ -47,3 +47,45 @@ def main()
                 rub - False
 
             all_spites_list.update()
+
+            screen.fill(black)
+
+            # bordas - parametros (surface, color, start_pos, end_pos, espessura)
+            pygame.draw.line(screen, grey, [8, 19], [width, 19], 48)
+            pygame.draw.line(screen, GREY, [(wall_width / 2) - 1, 0], [(wall_width / 2) - 1, HEIGHT], wall_width)
+            pygame.draw.line(screen, GREY, [(WIDTH - wall_width / 2) - 1, 0], [(WIDTH - wall_width / 2) - 1, HEIGHT],
+                             wall_width)
+
+            pygame.draw.line(screen, BLUE, [(wall_width / 2) - 1, HEIGHT - 65 + paddle_height / 2 - 54 / 2],
+                             [(wall_width / 2) - 1, HEIGHT - 65 + paddle_height / 2 - 54 / 2 + 54], wall_width)
+            pygame.draw.line(screen, BLUE, [(WIDTH - wall_width / 2) - 1, HEIGHT - 65 + paddle_height / 2 - 54 / 2],
+                             [(WIDTH - wall_width / 2) - 1, HEIGHT - 65 + paddle_height / 2 - 54 / 2 + 54], wall_width)
+
+            pygame.draw.line(screen, RED, [(wall_width / 2) - 1, 212.5],
+                             [(wall_width / 2) - 1, 212.5 + 2 * brick_height + 2 * y_gap], wall_width)
+            pygame.draw.line(screen, RED, [(WIDTH - wall_width / 2) - 1, 212.5],
+                             [(WIDTH - wall_width / 2) - 1, 212.5 + 2 * brick_height + 2 * y_gap], wall_width)
+
+            pygame.draw.line(screen, ORANGE, [(wall_width / 2) - 1, 212.5 + 2 * brick_height + 2 * y_gap],
+                             [(wall_width / 2) - 1, 212.5 + 4 * brick_height + 4 * y_gap], wall_width)
+            pygame.draw.line(screen, ORANGE, [(WIDTH - wall_width / 2) - 1, 212.5 + 2 * brick_height + 2 * y_gap],
+                             [(WIDTH - wall_width / 2) - 1, 212.5 + 4 * brick_height + 4 * y_gap], wall_width)
+
+            pygame.draw.line(screen, GREEN, [(wall_width / 2) - 1, 212.5 + 4 * brick_height + 4 * y_gap],
+                             [(wall_width / 2) - 1, 212.5 + 6 * brick_height + 6 * y_gap], wall_width)
+            pygame.draw.line(screen, GREEN, [(WIDTH - wall_width / 2) - 1, 212.5 + 4 * brick_height + 4 * y_gap],
+                             [(WIDTH - wall_width / 2) - 1, 212.5 + 6 * brick_height + 6 * y_gap], wall_width)
+
+            pygame.draw.line(screen, YELLOW, [(wall_width / 2) - 1, 212.5 + 6 * brick_height + 6 * y_gap],
+                             [(wall_width / 2) - 1, 212.5 + 8 * brick_height + 8 * y_gap], wall_width)
+            pygame.draw.line(screen, YELLOW, [(WIDTH - wall_width / 2) - 1, 212.5 + 6 * brick_height + 6 * y_gap],
+                             [(WIDTH - wall_width / 2) - 1, 212.5 + 8 * brick_height + 8 * y_gap], wall_width)
+
+            all_spites_list.draw(screen)
+
+            pygame.display.update()
+
+        pygame.quit()
+
+
+    main()
